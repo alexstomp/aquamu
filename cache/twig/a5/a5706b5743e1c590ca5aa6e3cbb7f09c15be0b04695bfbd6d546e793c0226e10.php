faa958144390f94b365e82349c2b525f8ba7d664
Alex Stomp <stompy1208@gmail.com>
2016-11-04 10:09:18 -0500

    dat cache

---
+++ cache/twig/a5/a5706b5743e1c590ca5aa6e3cbb7f09c15be0b04695bfbd6d546e793c0226e10.php
@@ -0,0 +1,34 @@
+<?php
+
+/* forms/fields/ignore/ignore.html.twig */
+class __TwigTemplate_7fe8aa8dafebc490902ac3ab56cc9b97112d569f065975b3ed2f30f975a11543 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+    }
+
+    public function getTemplateName()
+    {
+        return "forms/fields/ignore/ignore.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array ();
+    }
+
+    public function getSource()
+    {
+        return "";
+    }
+}
